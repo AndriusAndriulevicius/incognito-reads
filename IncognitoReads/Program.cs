68546d958c2834a5c0db15124063feed2c30ad9c
Andrius <andrius.andriulevicius15@gmail.com>
2025-03-09 22:19:41 +0200

    Front endo ir backendo sujungimas

--- IncognitoReads/Program.cs
+++ IncognitoReads/Program.cs
@@ -1,11 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();  
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy.WithOrigins("https://incognitoreads20250306223524.azurewebsites.net/") 
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -17,18 +26,16 @@ else
 }
 
 app.UseHttpsRedirection();
-
-// Enable static files (CSS, JS, images) from the wwwroot folder
-app.UseStaticFiles();  // This enables serving static files from wwwroot
-
+app.UseStaticFiles();
 app.UseRouting();
-
-// Add authorization middleware if required
+app.UseCors("AllowFrontend"); 
 app.UseAuthorization();
 
-// Set up the default routing for MVC
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllers(); 
 
 app.Run();
