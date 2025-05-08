8c06d0b85d86dd3f903416a42d46103380487cb6
Andrius <andrius.andriulevicius15@gmail.com>
2025-05-08 10:13:33 +0300

    Megstamiausios knygos pridejimas

--- incognito-reads-main/obj/Debug/net9.0/IncognitoReads.AssemblyInfo.cs
+++ incognito-reads-main/obj/Debug/net9.0/IncognitoReads.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IncognitoReads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0618e37855f65f00929fc60d3ca25b3cd03441e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("IncognitoReads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IncognitoReads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
